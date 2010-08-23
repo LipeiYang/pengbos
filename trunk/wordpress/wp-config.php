8d1fb773912c8978d594c6938e33da36dee10683
pengbos.com <pengbos.com@610e1dd1-8d48-5c67-b1eb-3ebc5c739462>
2010-08-23 16:14:08 +0000

    change password

--- trunk/wordpress/wp-config.php
+++ trunk/wordpress/wp-config.php
@@ -22,7 +22,7 @@ define('DB_NAME', 'pentan4_ei');
 define('DB_USER', 'root');
 
 /** MySQL database password */
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', '123$qweR');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
