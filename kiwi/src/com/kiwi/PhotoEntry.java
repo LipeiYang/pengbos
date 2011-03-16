306d7860d3bcb2b78911747f7832a208fb1d00d2
pengbos.com <pengbos.com@610e1dd1-8d48-5c67-b1eb-3ebc5c739462>
2011-03-16 15:38:27 +0000

    init

---
+++ kiwi/src/com/kiwi/PhotoEntry.java
@@ -0,0 +1,27 @@
+package com.kiwi;
+
+public class PhotoEntry {
+	
+	String title;
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getUrl() {
+		return url;
+	}
+	public void setUrl(String url) {
+		this.url = url;
+	}
+	public String getThumbUrl() {
+		return thumbUrl;
+	}
+	public void setThumbUrl(String thumbUrl) {
+		this.thumbUrl = thumbUrl;
+	}
+	String url;
+	String thumbUrl;
+
+}
